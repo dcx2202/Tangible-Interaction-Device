e8f9d5be55899ae578333d6f24ab657119935cef
Diogo Cruz <31130828+dcx2202@users.noreply.github.com>
2019-12-03 12:45:31 +0000

    Add files via upload

--- arduino_sketch.ino
+++ arduino_sketch.ino
@@ -5,25 +5,25 @@
 // Pins
 #define LED_RING_PIN 6 // Led ring pin
 #define BUTTON_PIN 7 // Button pin (input)
-//#define fsrpin A0 // Force sensor pin
 
 // Constants
 #define NUM_PIXELS 24 // NeoPixel ring LED count
-#define NUM_LOOP_SKIPS 100 // Number of loops to skip before updating the breathing effect ; higher = slower breathing effect
-#define WAIT_TIME 400 // Time to wait after the "exhale" phase of the breathing effect ; higer = longer
-#define HUE 370 // Hue of the pink color
+#define EXHALE_WAIT_TIME 5000 // Time to wait (in milliseconds) after the "exhale" phase of the breathing effect (brightness fades to 0%) ; higher = longer
+#define BREATHING_SPEED 8000 // Duration (in milliseconds) of breathing effect (0% brightness to 100% back to 0%) ; higher = slower
+#define PINK_HUE 370 // Hue of the pink color
 
 // Hardware objects
 Adafruit_DRV2605 drv; // Vibration motor
 Adafruit_NeoPixel pixels(NUM_PIXELS, LED_RING_PIN, NEO_GRB + NEO_KHZ800); // Setting up the NeoPixel library
 
 // Auxiliary Variables
-long loop_count; // Total loop count
+long lastBreathingUpdate; // Time of last breathing effect update
+long lastExhale; // Time of last exhale
+long loopCount; // Total number of loops
 bool increasing; // Breathing effect direction
-int current_brightness; // Breathing effect current brightness
-int current_wait_time; // Time left to wait after the "exhale" phase of the breathing effect
-int heart_rate; // Time to wait between heartbeats
-int heart_pulses_interval; // Time to wait between each pulse in a heartbeat
+int currentBrightness; // Breathing effect current brightness
+int heartRate; // Time to wait (in milliseconds) between heartbeats
+int heartPulsesInterval; // Time to wait (in milliseconds) between each pulse in a heartbeat
 
 void setup()
 {
@@ -41,70 +41,66 @@ void setup()
   drv.setMode(DRV2605_MODE_INTTRIG); 
 
   pixels.begin(); // Initialize NeoPixel strip object
-  pixels.setBrightness(250); // Set starting brightness at 100%
+  pixels.setBrightness(100); // Set starting brightness at 100%
 
   // Initialize auxiliary variables
-  loop_count = 0;
+  lastBreathingUpdate = 0;
+  lastExhale = 0;
   increasing = true;
-  current_brightness = 0;
-  current_wait_time = 0;
-  heart_rate = 800;
-  heart_pulses_interval = 400;
+  currentBrightness = 0;
+  heartRate = 800;
+  heartPulsesInterval = 400;
 }
 
 void breathing_effect()
 {
-  // Limit the number of times we update the effect
-  if(loop_count % NUM_LOOP_SKIPS != 0)
-    return;
-
   // Update the effect
   // Wait for some time after the "exhale" phase if that's now
-  if(current_wait_time > 0)
-  {
-    current_wait_time--;
+  if(millis() - lastExhale < EXHALE_WAIT_TIME)
     return;
-  }
 
-  // First phase
+  // Limit the number of times we update the effect
+  if(millis() - lastBreathingUpdate < double(BREATHING_SPEED) * 10.0 / 5000.0)
+    return;
+
   if(increasing)
   {
-    // Increase the brightness until the max, then start decreasing
-    if(current_brightness < 255)
-      current_brightness++;
+    if(currentBrightness < 255)
+      currentBrightness++;
     else
     {
       increasing = false;
-      current_brightness--;
+      currentBrightness--;
     }
   }
-  // Second phase
   else
   {
-    // Decrease the brightness until the min, then start increasing
-    if(current_brightness > 0)
-      current_brightness--;
+    if(currentBrightness > 0)
+      currentBrightness--;
     else
     {
-      current_wait_time = WAIT_TIME;
       increasing = true;
-      current_brightness++;
+      currentBrightness++;
+      lastExhale = millis();
     }
   }
   
-  pixels.fill(pixels.ColorHSV(HUE, 255, current_brightness));
+  uint32_t color = pixels.ColorHSV(PINK_HUE, 255, currentBrightness);
+  pixels.fill(color);
   pixels.show();
+
+  lastBreathingUpdate = millis();
 }
 
 void heartbeat_effect()
 {
-  heart_pulses_interval = map(inoise16(loop_count, loop_count), 0, 65535, 300, 500);
-  heart_rate = map(inoise16(loop_count, loop_count), 0, 65535, 400, 1200);
+  heartPulsesInterval = map(inoise16(loopCount, loopCount), 0, 65535, 300, 500);
+  heartRate = map(inoise16(loopCount, loopCount), 0, 65535, 400, 1200);
 }
 
 void loop()
 {
-  loop_count++;
+  loopCount++;
   heartbeat_effect();
   
   // If the button isn't being pressed then the user
@@ -116,11 +112,10 @@ void loop()
   }
 
   increasing = false;
-  current_brightness = 255;
-  current_wait_time = 0;
+  currentBrightness = 255;
   
   // If the user is handling the device we turn on the leds and play an heartrate vibration effect
-  pixels.fill(pixels.ColorHSV(HUE, 255, 255));
+  pixels.fill(pixels.ColorHSV(PINK_HUE, 255, 255));
   pixels.show();
 
   // Set the heartrate vibration effect
@@ -129,12 +124,12 @@ void loop()
   drv.setWaveform(1, 0);
   drv.go();
 
-  delay(heart_pulses_interval);
+  delay(heartPulsesInterval);
 
   // Second pulse
   drv.setWaveform(0, 48);
   drv.setWaveform(1, 0);
   drv.go();
 
-  delay(heart_rate);
+  delay(heartRate);
 }
